f2f9d4e53fd37a11560d070717c4e26b3a7f0294
Karimov Eli <kaimidze@gmail.com>
2018-01-19 15:40:15 +0400

    Doors open sound, assets update

---
+++ Assets/Scripts/DoorOpenSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenSound : MonoBehaviour
+{
+
+    public AudioClip doorOpenSound;
+
+    private AudioSource source;
+    private float currentAngle;
+    private bool opened;
+    // Use this for initialization
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        opened = false;
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (!opened)
+        {
+            source.PlayOneShot(doorOpenSound, 0.4F);
+        }
+        opened = true;
+    }
+}
