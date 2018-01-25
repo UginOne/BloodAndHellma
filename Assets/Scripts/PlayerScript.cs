4f96569e5a8c5d6287f51cc15440f48e2bcf22dc
Andrei Trifonov <a.trifonov@elewise.com>
2018-01-25 11:32:58 +0400

    - MainMenu

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,7 +11,8 @@ public class PlayerScript : MonoBehaviour {
 
     // Use this for initialization
     void Start()
-    {                                      
+    {
+        Debug.Log(Global.login);
     }
 
     // Update is called once per frame
